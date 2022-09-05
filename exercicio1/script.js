2aa802bef967b0ceefbc0512025e96e61ba74c9b
vitorfontenele <vitor_fontenele@hotmail.com>
2022-09-05 13:44:23 -0300

    Exercícios 1 e 2 resolvidos

---
+++ exercicio1/script.js
@@ -0,0 +1,27 @@
+let sacolao = [];
+
+//Frutas
+const banana = {
+  nome: "banana",
+  preco: 2.99,
+  disponibilidade: true,
+};
+
+const mamao = {
+  nome: "mamao",
+  preco: 4.99,
+  disponibilidade: true,
+};
+
+const morango = {
+  nome: "morango",
+  preco: 5.99,
+  disponibilidade: true,
+};
+
+//Adicionando frutas ao array
+sacolao.push(banana, mamao, morango);
+
+//Impressao
+console.log(sacolao);
+
