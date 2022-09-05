21bce8c9e53b604c01a1d14fc4ed4d8b599849ab
vitorfontenele <vitor_fontenele@hotmail.com>
2022-09-05 14:01:19 -0300

    Exercício 3 resolvido

---
+++ exercicio3/script.js
@@ -0,0 +1,44 @@
+const pokemon1 = {
+	nome: "Bulbasaur",
+	tipo: "Grama",
+	nivel: 5
+};
+
+//a)
+const pokemon1Copia = {
+    ...pokemon1,
+    nome: "Squirtle",
+    tipo: "Água"
+};
+
+//b)
+pokemon1.ataques = [];
+pokemon1.ataques.push({
+    nome: "Investida",
+    dano: 40,
+    tipo: "Normal",
+    precisao: 100
+});
+
+//c)
+pokemon1Copia.ataques = [...pokemon1.ataques];
+
+//d)
+pokemon1.ataques.push({
+    nome: "Folha Navalha",
+    dano: 45,
+    precisao: 100,
+    tipo: "Grama"
+});
+
+//e)
+pokemon1Copia.ataques.push({
+   nome: "Jato de Água",
+   dano: 40,
+   precisao: 100,
+   tipo: "Água"
+});
+
+//f)
+console.log(pokemon1);
+console.log(pokemon1Copia);
